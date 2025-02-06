46097413a6887f651ed07348558250973b825d17
ThibautG <thibaut.gaudinat@gmail.com>
2025-02-06 12:22:56 +0100

    création branche dev + premières routes dans endpoint.js

--- endpoint.js
+++ endpoint.js
@@ -38,6 +38,7 @@ router.get("/produits/:id", (req, res) => {
     });
 });
 
+
 /*
 * Route : inscription d'un client
 * POST /api/clients/register
@@ -83,13 +84,106 @@ router.post("/clients/register", (req, res) => {
 
                 res.status(201).json({message: "Inscription réussie", client_id: result.insertId});
             },
-            );
+        );
     });
 });
 
 
 
+/*
+* Route : récupérer un client par son id
+* GET /api/clients/:id
+* Exemple : GET /api/clients/3
+* */
+router.get("/clients/:id", (req, res) => {
+    const { id } = req.params; // pareil que const id = req.params.id
+    db.query("SELECT * FROM client WHERE Identifiant_client = ?", [id], (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: "Erreur du serveur" });
+        }
 
+        if (result.length === 0) {
+            return res.status(404).json({ message: "Client non trouvé" });
+        }
+
+        res.json(result[0]); // retournera uniquement le premier résultat
+    });
+});
+
+
+
+/*
+* Route : modification d'un compte client avec son id
+* PUT /api/clients/:id
+* Exemple : JSON
+* {
+* "Nom_client" : "Dupont",
+* "Prenom_client" : "Jean",
+* "Date_inscription_client": "2025-02-13",
+* "Mail_client" : "jean.dupont@email.com",
+* "Telephone_client": "0793625147",
+* "Adresse_client": "13 Rue Des Roses, Nice, 06004",
+* "Mdp_client" : "monMotDePasse"
+* }
+* */
+router.put("/clients/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const { mail, tel, adresse } = req.body;
+
+    db.query("UPDATE client SET Mail_client = ?, Telephone_client = ?, Adresse_client = ? WHERE Identifiant_client = ?", [mail, tel, adresse, id], (err, result) => {
+        if (err) {
+            // console.log(err);
+            return res.status(500).json({message: "Erreur lors de la modification"});
+        }
+
+        res.status(201).json({message: "Modification réussie"});
+        }
+    );
+
+
+});
+
+
+/*
+* Route : liste des commandes du client
+* GET /api/commandes/clients/:id
+* Exemple : GET /api/commandes/clients/3
+* */
+router.get("/commandes/clients/:id", (req, res) => {
+    const { id } = req.params; // pareil que const id = req.params.id
+    db.query("SELECT Identifiant_commande, Date_commande, Montant_ttc_commande FROM commande WHERE Identifiant_client = ?", [id], (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: "Erreur du serveur" });
+        }
+
+        if (result.length === 0) {
+            return res.status(404).json({ message: "Le client n'a passé aucune commande" });
+        }
+
+        res.json(result);
+    });
+});
+
+
+/*
+* Route : liste d'une commande d'un client
+* GET /api/commandes/:id
+* Exemple : GET /api/commandes/3
+* */
+router.get("/commandes/:id", (req, res) => {
+    const { id } = req.params; // pareil que const id = req.params.id
+    db.query("SELECT * FROM commande WHERE Identifiant_commande = ?", [id], (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: "Erreur du serveur" });
+        }
+
+        if (result.length === 0) {
+            return res.status(404).json({ message: "Commande non trouvée" });
+        }
+
+        res.json(result);
+    });
+});
 
 
 
