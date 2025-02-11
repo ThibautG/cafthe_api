4d85433a497a3255fa6a4a4a7de7a04b3576ea1f
ThibautG <thibaut.gaudinat@gmail.com>
2025-02-11 16:14:20 +0100

    création du verifyToken dans middleware.js

--- middleware.js
+++ middleware.js
@@ -1,1 +1,22 @@
 // le middleware vient se caler avant une route et permet de bloquer l'accès à cette route selon les users
+const jwt = require("jsonwebtoken");
+
+// création du middleware
+const verifyToken = (req, res, next) => {
+    const token = req.header["authorization"];
+
+    if (!token) {
+        return res.status(403).json({message: "Token introuvable"});
+    }
+
+    jwt.verify(token.split(" ")[1], process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({message: "Token invalide"});
+        }
+
+        req.client = decoded;
+        next();
+    });
+};
+
+module.exports = { verifyToken };
